914dfa7379d2feefc120c6a8afb7f757cb8b8fbd
Derek Boone <boonesoftware@gmail.com>
2015-07-28 19:29:19 -0400

    Add `Entry` Detail and List views
    The Entry List view displays a list of Entries with corresponding URLs
    to the Entry Detail view.

--- log/urls.py
+++ log/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
-
+from django.views.generic.list import ListView
 from . import views
+from models import Entry
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', ListView.as_view(
+		model=Entry,
+		paginate_by='10',
+		queryset=Entry.objects.all(),
+		context_object_name='entries',
+		template_name='entry_list.html')),
+	url(r'^entries/([0-9]+)', views.entry_detail),
 ]
--- log/views.py
+++ log/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+from models import Entry
 
 def index(request):
 	return HttpResponse("Hello, world. You're at the log index.")
+	
+def entry_detail(request, key="1"):
+	try:
+		entry = Entry.objects.get(pk=key)
+		return render(request, 'entry.html', {'entry' : entry})
+	except Entry.DoesNotExist:
+		raise Http404("Entry does not exist!")
